89911d3c99f34a78e882f1c0649a95143c15b51b
Ross Cumming <rosso5792@gmail.com>
2021-01-02 14:05:04 +0000

    figured out how to read json file, parse it, and print it

--- lib/currency_exchange.rb
+++ lib/currency_exchange.rb
@@ -1,10 +1,19 @@
+require 'json'
+require 'date'
+
 module CurrencyExchange
+  
+  @rates_file = File.read('./data/rates.json')
+  @rates_obj = JSON.parse(@rates_file)
+
+  p @rates_obj.keys
 
   # Return the exchange rate between from_currency and to_currency on date as a float.
   # Raises an exception if unable to calculate requested rate.
   # Raises an exception if there is no rate for the date provided.
   def self.rate(date, from_currency, to_currency)
     # TODO: calculate and return rate
+    date = date.strftime('%y-%m-%d')
   end
 
 end
