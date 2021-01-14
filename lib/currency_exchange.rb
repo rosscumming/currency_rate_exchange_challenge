7c6750b3d94092535f586a3cc4cf011da9c5f4a4
Ross <56764878+rosscumming@users.noreply.github.com>
2021-01-14 15:47:35 +0000

    Update currency_exchange.rb

--- lib/currency_exchange.rb
+++ lib/currency_exchange.rb
@@ -12,7 +12,7 @@ module CurrencyExchange
     
     raise "This date doesn't exist" if !@rates_obj[date] 
     raise "invalid input type" if !to_currency.kind_of? String 
-    raise "invalid input type" if !to_currency.kind_of? String 
+    raise "invalid input type" if !from_currency.kind_of? String 
     raise "There is no currency found on this date" if !@rates_obj[date][to_currency] || !@rates_obj[date][from_currency]
 
     @rates_obj[date][to_currency] / @rates_obj[date][from_currency]
